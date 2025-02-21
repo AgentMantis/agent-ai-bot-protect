cae6891d106e45ceb83b0d2a2bbbd358389dc34a
Anthony <ajbarry99@gmail.com>
2025-02-22 01:18:58 +1100

    menu work.

--- bot-shield-app/src/app/app.component.ts
+++ bot-shield-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewEncapsulation } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, RouterLink, MatTabsModule, MatButtonModule],
+  imports: [RouterModule, RouterOutlet, CommonModule, RouterLink, MatTabsModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- bot-shield-app/src/app/components/settings/settings.component.ts
+++ bot-shield-app/src/app/components/settings/settings.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-settings',
-  imports: [MatTabsModule, MatButtonModule],
+  imports: [],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
-  links = ['First', 'Second', 'Third'];
-  activeLink = this.links[0];
-  addLink() {
-    this.links.push(`Link ${this.links.length + 1}`);
-  }
+
 }
