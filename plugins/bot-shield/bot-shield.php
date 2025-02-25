47ca1cb678382ab5aaec5bb5f7c5b82079d95341
Anthony <ajbarry99@gmail.com>
2025-02-26 08:10:11 +1100

    tidyup.

--- plugins/bot-shield/bot-shield.php
+++ plugins/bot-shield/bot-shield.php
@@ -330,8 +330,9 @@ function bot_shield_admin_styles() {
     <style>
         #toplevel_page_bot-shield .wp-menu-image img {
             height: 25px;
-            margin-top: -5px;
-            margin-left: 8px;
+            padding: 0px;
+            margin-top: 3px;
+            margin-left: 7px;
         }
     </style>
     ';
